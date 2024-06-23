b66a3fea3bb3025dcec139c6a269854ac5e1e346
spksoft <spkrsk.37@gmail.com>
2024-06-23 16:19:14 +0700

    chore: Update npm dependencies and AI SDK package versions

--- nextjs/src/app/api/chat/route.ts
+++ nextjs/src/app/api/chat/route.ts
@@ -1,26 +1,17 @@
+import { type CoreMessage, streamText } from 'ai';
 import { openai } from '@ai-sdk/openai';
-import { StreamData, StreamingTextResponse, streamText } from 'ai';
 
 // Allow streaming responses up to 30 seconds
 export const maxDuration = 30;
 
 export async function POST(req: Request) {
-  const { messages } = await req.json();
+  const { messages }: { messages: CoreMessage[] } = await req.json();
 
   const result = await streamText({
     model: openai('gpt-4o'),
+    system: 'You are a helpful assistant.',
     messages,
   });
 
-  const data = new StreamData();
-
-  data.append({ test: 'value' });
-
-  const stream = result.toAIStream({
-    onFinal(_) {
-      data.close();
-    },
-  });
-
-  return new StreamingTextResponse(stream, {}, data);
+  return result.toAIStreamResponse();
 }
